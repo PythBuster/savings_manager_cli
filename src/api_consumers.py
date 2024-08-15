9db069fe9746f446784f0ca48cefccfe5f4389ca
PythBuster <pythbuster@gmail.com>
2024-08-15 12:28:27 +0200

    feat: move Endpoint enum type to custom_types.py

--- src/api_consumers.py
+++ src/api_consumers.py
@@ -1,50 +1,11 @@
 from abc import ABC
-from enum import StrEnum
 
 import requests
 import typer
 
 from src.config import BASE_URL, PORT
-from src.custom_types import MoveDirection
+from src.custom_types import MoveDirection, Endpoint
 from src.utils import colorize_number, exit_with_error, tabulate_str
-
-
-class Endpoint(StrEnum):
-    """The endpoint types."""
-
-    LIST_ALL_MONEYBOXES = "/api/moneyboxes"
-    """GET Endpoint for list all moneyboxes."""
-
-    LIST_SPECIFIC_MONEYBOX = "/api/moneybox"
-    """GET Endpoint for list specific moneybox."""
-
-    ADD_AMOUNT_TO_MONEYBOX = "/api/moneybox/{moneybox_id}/balance/add"
-    """POST Endpoint to add amount to specific moneybox."""
-
-    SUB_AMOUNT_TO_MONEYBOX = "/api/moneybox/{moneybox_id}/balance/sub"
-    """POST Endpoint to sub amount from specific moneybox."""
-
-    TRANSFER_AMOUNT = "/api/moneybox/{moneybox_id}/balance/transfer"
-    """POST Endpoint to transfer amount between specific moneyboxes."""
-
-    CREATE_MONEYBOX = "/api/moneybox"
-    """POST Endpoint to create moneybox."""
-
-    UPDATE_MONEYBOX = "/api/moneybox/{moneybox_id}"
-    """PATCH Endpoint to update specific moneybox."""
-
-    SHOW_MONEYBOX_LOGS = "/api/moneybox/{moneybox_id}/transactions"
-    """GET Endpoint for show moneybox logs."""
-
-    DELETE_MONEYBOX = "/api/moneybox/{moneybox_id}"
-    """DELETE Endpoint to delete a specific moneybox."""
-
-    GET_PRIORITYLIST = "/api/prioritylist"
-    """GET Endpoint for get prioritylist."""
-
-    UPDATE_PRIORITYLIST = "/api/prioritylist"
-    """UPDATE Endpoint for update prioritylist."""
-
 
 class ApiConsumerFactory(ABC):
     """Base class for all API consumers."""
